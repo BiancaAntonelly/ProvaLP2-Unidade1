6ebad805d5b390f5e4cef54f0939173a44694f2c
Vanessa Maria <vanessa.silva5205@gmail.com>
2023-09-22 21:54:09 -0300

    Ajuste na classe Main

--- ProvaLp/src/Projeto/Main.java
+++ ProvaLp/src/Projeto/Main.java
@@ -4,31 +4,17 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Main loja = new Main();
+        ArrayList<Produto> carrinho = new ArrayList<>();
 
         Mouse mouse1 = new Mouse("Mouse otico, Saida USB. 1.600 dpi", "otico");
         Livro livro1 = new Livro("Aventuras de Sherlock Holmes", "Arthur Conan Doyle");
         Mouse mouse2 = new Mouse("Mouse sem fio, 2.400 dpi", "Sem Fio");
 
-        loja.adicionarProduto(mouse1);
-        loja.adicionarProduto(livro1);
-        loja.adicionarProduto(mouse2);
+        carrinho.add(mouse1);
+        carrinho.add(livro1);
+        carrinho.add(mouse2);
 
-        loja.mostrarCarrinho();
-    }
-
-    private ArrayList<Produto> carrinho;
-
-    public Main() {
-        carrinho = new ArrayList<>();
-    }
-
-    public void adicionarProduto(Produto produto) {
-        carrinho.add(produto);
-    }
-
-    public void mostrarCarrinho() {
-        for (Produto produto : carrinho) {
+        for(Produto produto : carrinho) {
             System.out.println(produto.getDescricao());
         }
     }
